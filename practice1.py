4a0ff3e512595d58204ca452ff8e030b11bce0ea
Radhika kapoor <radhikakapoor383@gmail.com>
2026-03-29 16:30:56 +0500

    More practise of list and loops

--- practice1.py
+++ practice1.py
@@ -1,3 +1,19 @@
 passengers = ["Ali", "Sana", "Usman", "Hina"]
 print(passengers[-1])
+print(passengers[0])
+print(passengers[1:3])
+print(passengers)
 
+passengers.append("Bilal")
+print(passengers)
+passengers.remove("Usman")
+print(passengers)
+passengers.insert(1, "Fatima")
+i = 1
+for name in passengers:
+    print(f"Passenger {i}: {name}")
+    i+=1
+    
+ages = [22, 45, 17, 31, 8, 55]
+adults = [age for age in ages if age >= 18]
+print(adults)

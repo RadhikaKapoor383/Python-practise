ad7a3f481d7eb506e7b0a5afbde801973bd2ba8c
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-05 19:24:09 +0500

    practicing list

---
+++ lists.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "mango", "banana", "grape", "orange"]
+numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+mixed = [1, "ali", True, 3.14]
+empty = []
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+print(fruits[-2])
+
+print(len(fruits))

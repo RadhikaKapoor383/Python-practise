e52ff6fc0b040d5b6fb555d57ae9ac74ed497a93
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-05 19:39:36 +0500

    Modifying the existing list

--- lists.py
+++ lists.py
@@ -1,11 +1,32 @@
 fruits = ["apple", "mango", "banana", "grape", "orange"]
-numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+num = [10, 20, 30, 40, 50, 60, 70, 80]
 mixed = [1, "ali", True, 3.14]
 empty = []
-print(fruits)
-print(fruits[0])
-print(fruits[1])
-print(fruits[-1])
-print(fruits[-2])
+# print(fruits)
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[-1])
+# print(fruits[-2])
 
-print(len(fruits))
+# print(len(fruits))
+
+# print(num[1:4])
+# print(num[:3])
+# print(num[3:])
+# print(num[1::4])
+# print(num[::2])
+# print(num[::-1])
+# print(num[::1])
+
+fruits = ["apple", "mango", "banana", "grape", "orange"]
+fruits[1] = "kiwi"
+print(fruits)
+
+fruits.append("pineapple")
+print(fruits)
+fruits.insert(2, "papaya")
+print(fruits)
+fruits.remove("kiwi")
+print(fruits)
+fruits.insert(1, "mango")
+print(fruits)

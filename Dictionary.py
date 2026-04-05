d7e2ca5cb2085e8ee0444ebe6f44a1a95e622336
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-05 22:18:26 +0500

    created a student dictionary and

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+# Dictionary is key and value thing
+student = {
+    "name": "Radhika kapoor",
+    "age": 20,
+    "course": ["Eng", "maths", "urdu"],
+    "marks": [80, 90, 88],
+    "dept": "CS"
+}
+print(student["marks"][0])
+print(student.get("gpa"))
+print(student.get("gpa"), 0.0)
+student["gpa"] = 3.33
+student["city"] = "Sukkur"
+student["age"] = 21
+print(student)
+
+# del student["city"]            # key hatao
+# popped = student.pop("age")    # hatao aur value return karo
+# print(student)

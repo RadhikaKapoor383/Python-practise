9fc46cb2d5c055d9c6a4dea9f8c930e574b09285
Radhika kapoor <radhikakapoor383@gmail.com>
2026-04-08 23:44:28 +0500

    add 2D arr and some np functions

--- NumPy.py
+++ NumPy.py
@@ -2,3 +2,12 @@ import numpy as np
 # Create a 1D array
 arr1 = np.array([1, 2, 3, 4, 5])
 print("1D Array:", arr1)
+arr2 = np.array([[1, 2, 3, 4, 5]])
+print("2D Array:", arr2)
+print("Shape of arr1:", arr1.shape)
+print("Shape of arr2:", arr2.shape)
+print("Square root of 4:", np.sqrt(4))
+print("Sine of 90 degrees:", np.sin(np.radians(90)))
+print("Cosine of 0 degrees:", np.cos(np.radians(0)))
+print("Exponential of 1:", np.exp(1))
+print("Logarithm of 10:", np.log(10))
